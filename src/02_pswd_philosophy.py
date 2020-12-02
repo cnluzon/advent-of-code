d98bb32055cd978889f101a68fddf6bccc033ee3
C. Navarro <cnluzon@gmail.com>
2020-12-02 18:24:21 +0100

    Refactor Policy

--- src/02_pswd_philosophy.py
+++ src/02_pswd_philosophy.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 import re
 
+
 @dataclass
 class Policy:
   min_count: int
@@ -8,23 +9,29 @@ class Policy:
   letter: str
 
 
+class SledPolicy(Policy):
+  def validate(self, password):
+    counts = password.count(self.letter)
+    return (counts >= self.min_count and counts <= self.max_count)
+
+
+class TobogganPolicy(Policy):
+  def validate(self, password):
+    return (password[self.min_count-1] == self.letter) ^ \
+           (password[self.max_count-1] == self.letter)
+
+
 @dataclass
 class Password:
   policy: Policy
   value: str
-
+  
   @property
   def is_valid(self):
-    counts = self.value.count(self.policy.letter)
-    return (counts >= self.policy.min_count and counts <= self.policy.max_count)
+    return self.policy.validate(self.value)
 
-  @property
-  def is_valid_new(self):
-    return (self.value[self.policy.min_count-1] == self.policy.letter) ^ \
-           (self.value[self.policy.max_count-1] == self.policy.letter)
-  
 
-def parse_password(s):
+def parse_password(s, policy_type):
   """Parse a password from a given string"""
   match = re.match(r"(\d+)-(\d+)\s(\w):\s+(\w+)", s)
 
@@ -32,24 +39,26 @@ def parse_password(s):
     min_count = int(match.group(1))
     max_count = int(match.group(2))
     letter = match.group(3)
-  
     passwd = match.group(4)
   
-    return Password(
-      Policy(min_count, max_count, letter),
-      passwd
-    )
+    policy = policy_type(min_count, max_count, letter)
+    return(Password(policy, passwd))
 
   return None
 
 
-if __name__ == "__main__":
+def read_passwords(file, policy_type=SledPolicy):
   passwds = []
-  with open("../data/02.txt") as fi:
-    passwds = [parse_password(s) for s in fi.readlines()]
+  with open(file) as fi:
+    passwds = [parse_password(s, policy_type=policy_type) for s in fi.readlines()]
+  return passwds
 
+
+if __name__ == "__main__":
+  passwds = read_passwords("../data/02.txt", policy_type=SledPolicy)
   valid_pass_count = sum(p.is_valid for p in passwds if p)
   print(f"Valid passwords: {valid_pass_count}")
   
-  valid_pass_count_new = sum(p.is_valid_new for p in passwds if p)
-  print(f"New valid passwords: {valid_pass_count_new}")
+  passwds = read_passwords("../data/02.txt", policy_type=TobogganPolicy)
+  valid_pass_count = sum(p.is_valid for p in passwds if p)
+  print(f"New valid passwords: {valid_pass_count}")
