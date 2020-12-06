8a7a749049846f72b3caece8390c37510619d6db
C. Navarro <cnluzon@gmail.com>
2020-12-06 11:15:37 +0100

    Move split function to common

---
+++ src/common.py
@@ -0,0 +1,9 @@
+def split(sequence, sep):
+    chunk = []
+    for val in sequence:
+        if val == sep:
+            yield chunk
+            chunk = []
+        else:
+            chunk.append(val)
+    yield chunk
