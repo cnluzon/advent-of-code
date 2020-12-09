e26908e1d1f7b685aa2a5d92b1d335b65c2b208b
C. Navarro <cnluzon@gmail.com>
2020-12-09 16:46:00 +0100

    Second part of the solution

--- src/09_encoding_error.py
+++ src/09_encoding_error.py
@@ -6,19 +6,39 @@ from typing import List
 @dataclass
 class XMASValidator:
     values: List[int]
+    window: List[int]
 
     def add_number(self, n):
         if self.is_valid(n):
-            self.values = self.values[1:] + [n]
+            self.window = self.window[1:] + [n]
             return True
         else:
             return False
 
     def is_valid(self, n):
-        for pick in itertools.combinations(self.values, 2):
+        for pick in itertools.combinations(self.window, 2):
             if len(set(pick)) == len(pick) and sum(pick) == n:
                 return True
         return False
+
+    def find_contiguous_sum(self, n):
+        for i in range(len(self.values)):
+            sum_values = self.contiguous_sum_ahead(i, n)
+            if sum_values:
+                return sum_values
+
+    def contiguous_sum_ahead(self, start, n):
+        accum = 0
+        i = start
+        while accum < n and i <= len(self.values):
+            accum += self.values[i]
+            i += 1
+
+        if accum == n:
+            return self.values[start: i]
+        else:
+            return None
+
 
 
 def read_input_data(filename):
@@ -33,8 +53,10 @@ if __name__ == "__main__":
     preamble_length = 25
     preamble = values[0:preamble_length]
 
-    xmas_validator = XMASValidator(preamble)
+    xmas_validator = XMASValidator(values, preamble)
     for v in values[preamble_length:]:
         if not xmas_validator.add_number(v):
             print(v)
+            sum_vals = xmas_validator.find_contiguous_sum(v)
+            print(min(sum_vals) + max(sum_vals))
             exit()
