dc7a340f1c222da2ed3dae5b48319d002aebad47
C. Navarro <cnluzon@gmail.com>
2020-12-14 12:14:43 +0100

    Second part of day 14

--- src/14_docking_data.py
+++ src/14_docking_data.py
@@ -29,13 +29,45 @@ def parse_instruction(s) -> Instruction:
 
 def mask_value(int_value, mask):
     bin_str = bin(int_value)[2:]
-    binary_value = list('0'*(len(mask)-len(bin_str)) + bin_str)
+    binary_value = list('0' * (len(mask) - len(bin_str)) + bin_str)
     # cut_mask = mask[-len(binary_value):]
     for i, b in enumerate(mask):
         if b != 'X':
             binary_value[i] = b
 
     return int(''.join(binary_value), 2)
+
+
+def binary_generator(n):
+    for i in range(0, 2 ** n):
+        bin_val = bin(i)[2:]
+        yield "0" * (n - len(bin_val)) + bin_val
+
+
+def all_binary_values(masked_value):
+    gen = binary_generator(masked_value.count('X'))
+    result = []
+    for g in gen:
+        g_index = 0
+        new_masked_value = masked_value[:]
+        for b_index, b in enumerate(new_masked_value):
+            if b == 'X':
+                new_masked_value[b_index] = g[g_index]
+                g_index += 1
+
+        yield int(''.join(new_masked_value), 2)
+
+
+def compute_all_addresses(int_value, mask):
+    # 1. Fix the corresponding 1s and 0s in the mask
+    bin_str = bin(int_value)[2:]
+    binary_value = list('0' * (len(mask) - len(bin_str)) + bin_str)
+    for i, b in enumerate(mask):
+        if b != '0':
+            binary_value[i] = b
+
+    # 2. Combinatorially fill in all possible values
+    return all_binary_values(binary_value)
 
 
 def read_input_data(filename) -> List[Instruction]:
@@ -45,8 +77,8 @@ def read_input_data(filename) -> List[Instruction]:
 
 if __name__ == "__main__":
     instructions = read_input_data("../data/14.txt")
-    print(instructions)
 
+    # Version 1 decoder
     memory = {}
     current_mask = instructions[0].value
     for index, instr in enumerate(instructions[1:], 1):
@@ -55,5 +87,18 @@ if __name__ == "__main__":
         else:
             masked_value = mask_value(instr.value, current_mask)
             memory[instr.position] = masked_value
+
+    print(sum(memory.values()))
+
+    # Version 2 decoder
+    memory = {}
+    current_mask = instructions[0].value
+    for index, instr in enumerate(instructions[1:], 1):
+        if instr.type == "mask":
+            current_mask = instr.value
+        else:
+            all_addresses = compute_all_addresses(instr.position, current_mask)
+            for p in all_addresses:
+                memory[p] = instr.value
 
     print(sum(memory.values()))
